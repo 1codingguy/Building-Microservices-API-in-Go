a347d58926716acb777314ae29b761a4c604435c
1codingguy <1codingguy@gmail.com>
2024-02-24 14:03:32 +0700

    encode and send json in correct encoding

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	ZipCode string `json:"zip_code"`
+}
+
 func main() {
 	// user standard http library instead of external libraries
 
 	// function to register route
-	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
-		// send the response back to the client
-		// Fprintf takes the "hello world" string, write it to w
-		fmt.Fprint(w, "hello world")
-	})
+	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	// function to start server
 	// nil because relying on the default multiplexer instead of creating one our own
@@ -24,4 +28,28 @@ func main() {
 		log.Fatal(err)
 	}
 
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	// send the response back to the client
+	// Fprintf takes the "hello world" string, write it to w
+	fmt.Fprint(w, "hello world")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{
+			Name:    "John",
+			City:    "Phuket",
+			ZipCode: "83000",
+		},
+		{
+			Name:    "Peter",
+			City:    "Bangkok",
+			ZipCode: "10000",
+		},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
