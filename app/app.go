e50d698f2be8555be4a5d43d3b90e4388ab6e61e
1codingguy <1codingguy@gmail.com>
2024-02-25 12:43:09 +0700

    replace default mux with gorilla mux

--- app/app.go
+++ app/app.go
@@ -3,18 +3,21 @@ package app
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
 
 	// create our own multiplexer
-	mux := http.NewServeMux()
+	router := mux.NewRouter()
 
 	// function to register route
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	// function to start server
 	// nil because relying on the default multiplexer instead of creating one our own
-	log.Fatal(http.ListenAndServe("localhost:9000", mux))
+	log.Fatal(http.ListenAndServe("localhost:9000", router))
 }
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.21.5
 replace github.com/1codingguy/go-microservice-api/banking/app v0.0.0 => ./app
 
 require github.com/1codingguy/go-microservice-api/banking/app v0.0.0
+
+require github.com/gorilla/mux v1.8.1 // indirect
