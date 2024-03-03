996a591f287778886e4467cfa179be845d55d1b3
1codingguy <1codingguy@gmail.com>
2024-03-03 17:37:56 +0700

    domain and repository DB adapter for Account domain

--- app/app.go
+++ app/app.go
@@ -5,11 +5,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"banking/domain"
 	"banking/service"
 
 	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
 
 	"github.com/joho/godotenv"
 )
@@ -29,7 +31,11 @@ func Start() {
 	// service.NewService() - service refers to the package, not Service struct
 	// CustomerRepositoryStub implements CustomerRepository interface since it has a FindAll()
 	// ch := CustomerHandlers{service: service.NewService(domain.NewCustomerRepositoryStub())}
-	ch := CustomerHandlers{service: service.NewService(domain.NewCustomerRepositoryDb())}
+
+	dbClient := getDbClient()
+	customerRepositoryDb := domain.NewCustomerRepositoryDb(dbClient)
+	// accountRepositoryDb := domain.NewAccountRepositoryDB(dbClient)
+	ch := CustomerHandlers{service: service.NewService(customerRepositoryDb)}
 
 	// function to register route
 	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
@@ -42,4 +48,25 @@ func Start() {
 	port := os.Getenv("SERVER_PORT")
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", server, port), router))
+}
+
+// create one connection pool, used by different repositories
+func getDbClient() *sqlx.DB {
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbAddress := os.Getenv("DB_ADDRESS")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dataSource := fmt.Sprintf("%s:%s@(%s:%s)/%s", dbUser, dbPassword, dbAddress, dbPort, dbName)
+	client, err := sqlx.Open("mysql", dataSource)
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	client.SetConnMaxLifetime(time.Minute * 3)
+	client.SetMaxOpenConns(10)
+	client.SetMaxIdleConns(10)
+
+	return client
 }
--- domain/customerRepoDb.go
+++ domain/customerRepoDb.go
@@ -4,9 +4,6 @@ import (
 	"banking/errs"
 	"banking/logger"
 	"database/sql"
-	"fmt"
-	"os"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -55,23 +52,6 @@ func (d CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError
 	return customers, nil
 }
 
-func NewCustomerRepositoryDb() CustomerRepositoryDb {
-
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbAddress := os.Getenv("DB_ADDRESS")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-
-	dataSource := fmt.Sprintf("%s:%s@(%s:%s)/%s", dbUser, dbPassword, dbAddress, dbPort, dbName)
-	client, err := sqlx.Open("mysql", dataSource)
-	if err != nil {
-		panic(err)
-	}
-	// See "Important settings" section.
-	client.SetConnMaxLifetime(time.Minute * 3)
-	client.SetMaxOpenConns(10)
-	client.SetMaxIdleConns(10)
-
-	return CustomerRepositoryDb{client: client}
+func NewCustomerRepositoryDb(dbClient *sqlx.DB) CustomerRepositoryDb {
+	return CustomerRepositoryDb{client: dbClient}
 }
